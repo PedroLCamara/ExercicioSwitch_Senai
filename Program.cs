2e38865f5ad4e7dfafd54f18fa2b3227cbf28f83
PedroLCamara <plcrlschool@gmail.com>
2021-05-12 18:19:45 -0300

    ExercicioSwitch_Senai

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ExercicioSwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é o seu nome?");
+            string nome = Console.ReadLine();
+            string letra = nome.Substring(0,1).ToUpper();
+
+            Console.WriteLine();
+
+            switch(letra){
+                case "A":
+                    Console.WriteLine("seu nome começa com a vogal "+letra+" de aranha!!!");
+                break;
+                case "E":
+                    Console.WriteLine("seu nome começa com a vogal "+letra+" de elefante!!!");
+                break;
+                case "I":
+                    Console.WriteLine("seu nome começa com a vogal "+letra+" de iguana!!!");
+                break;
+                case "O":
+                    Console.WriteLine("seu nome começa com a vogal "+letra+" de onça!!!");
+                break;
+                case "U":
+                    Console.WriteLine("seu nome começa com a vogal "+letra+" de urso!!!");
+                break;
+                default:
+                    Console.WriteLine("Seu nome não começa com uma vogal :(");
+                break;
+            }
+        }
+    }
+}
